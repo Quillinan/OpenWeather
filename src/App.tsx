cac6f2c6a479726cece688a57a84ecce3a29cccf
Quillinan <milton.quillinan@gmail.com>
2023-11-26 23:20:07 -0300

    feat: add resets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
 import TodayPage from "./pages/TodayPage";
+import ResetStyle from "./styles/ResetStyle";
 
 export default function App() {
   return (
     <>
+      <ResetStyle />
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
