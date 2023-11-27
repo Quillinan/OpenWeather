beee84081f0557ecb8b2a28791275473d0f01649
Quillinan <milton.quillinan@gmail.com>
2023-11-26 23:20:57 -0300

    feat: today page

--- src/pages/TodayPage.tsx
+++ src/pages/TodayPage.tsx
@@ -1,52 +1,22 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
+import styled from "styled-components";
+import Panel from "../components/Panel";
+import Principal from "../components/Principal";
 
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-
-export default function TodayPage() {
-  const [post, setPost] = useState<Post>({} as Post);
-
-  const updatePost = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/1`);
-      if (response.status === 200) {
-        setPost(response.data);
-      } else {
-        setPost({} as Post);
-      }
-    } catch (error) {
-      Swal.fire({
-        title: "Conection error",
-        text: "Error with API connection",
-        icon: "error",
-      });
-      console.log(error);
-    }
-  };
-
-  const setError = async () => {
-    Swal.fire({
-      title: "It lives!",
-      text: "Just an error to show Swal working",
-      icon: "success",
-    });
-  };
-
-  useEffect(() => {
-    updatePost();
-  }, []);
+const TodayPage: React.FC = () => {
   return (
-    <div>
-      <h1>Vite + ReactTS + Axios</h1>
-      <p>Post title: {post.title}</p>
-      <p>Post body: {post.body}</p>
-      <button onClick={() => setError()}>Show error</button>
-    </div>
+    <PageContainer>
+      <Panel />
+      <Principal />
+    </PageContainer>
   );
-}
+};
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+`;
+
+export default TodayPage;
